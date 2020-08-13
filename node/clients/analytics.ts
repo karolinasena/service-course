740b57a912d5ad04a1b5334a75f9127c66ed583d
Karolina Sena <karolina.sena16@gmail.com>
2020-08-12 23:28:48 -0300

    aprendendo mais sobre clients

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,3 +1,16 @@
-import { AppClient } from '@vtex/api'
+import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
 
-export default class Analytics extends AppClient {}
+export default class Analytics extends AppClient {
+    constructor(context: IOContext, options?: InstanceOptions) {
+        super('vtex.mocked-analytics@0.x', context, options)
+    }
+
+    public getLiveUsers(): Promise<LiveUsersProduct[]> { 
+        return this.http.get('_v/live-products');
+    }
+}
+
+interface LiveUsersProduct {
+    slug: string
+    liveUsers: number
+}
--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -1,9 +1,14 @@
 export async function analytics(ctx: Context, next: () => Promise<any>) {
   if (ctx.method.toUpperCase() === 'GET') {
+    const {
+      clients: { analytics },
+    } = ctx
     ctx.status = 200
     ctx.body = 'OK'
+    ctx.body = await analytics.getLiveUsers();
+    ctx.set('cache-control', 'no-cache');
 
-    ctx.set('cache-control', 'no-cache')
+    ctx.set('cache-control', 'no-cache');
   }
-  await next()
+  await next();
 }
