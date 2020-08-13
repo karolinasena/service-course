10c90032d3f0221518664e7df1c1275c8fe77542
Karolina Sena <karolina.sena16@gmail.com>
2020-08-12 23:34:40 -0300

    ajustando código em analytics.ts

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,4 +1,4 @@
-import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
+import { AppClient, InstanceOptions, IOContext} from '@vtex/api'
 
 export default class Analytics extends AppClient {
     constructor(context: IOContext, options?: InstanceOptions) {
